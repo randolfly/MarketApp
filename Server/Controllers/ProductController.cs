c0fe583e7b319d4bd9b7d7a902799ad23a58e38c
randolf <zrq21@mails.tsinghua.edu.cn>
2022-10-26 21:01:41 +0800

    add sqlite connect string support

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarketApp.Server.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using MarketApp.Shared;
+using Microsoft.EntityFrameworkCore;
 
 namespace MarketApp.Server.Controllers;
 
@@ -8,37 +10,17 @@ namespace MarketApp.Server.Controllers;
 [ApiController]
 public class ProductController : ControllerBase
 {
-    private static List<Product> _products = new()
+    private readonly DataContext _dataContext;
+
+    public ProductController(DataContext dataContext)
     {
-        new Product()
-        {
-            Id = 1,
-            Title = "A",
-            Description = "PDDDD",
-            Price = 9.99m,
-            ImageUrl = "https://chaoskey.oschina.io/notes/images/0109.jpg"
-        },
-        new Product()
-        {
-            Id = 2,
-            Title = "B",
-            Description = "PDDDD",
-            Price = 19.99m,
-            ImageUrl = "https://chaoskey.oschina.io/notes/images/0109.jpg"
-        },
-        new Product()
-        {
-            Id = 3,
-            Title = "C",
-            Description = "PDDDD",
-            Price = 29.99m,
-            ImageUrl = "https://chaoskey.oschina.io/notes/images/0109.jpg"
-        }
-    };
+        _dataContext = dataContext;
+    }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
     {
-        return Ok(_products);
+        var products = await _dataContext.Products.ToArrayAsync();
+        return Ok(products);
     }
 }
